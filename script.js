a716b37efaa1c148310bf4494d564195c10a627c
Ritul <01fe22bca126@kletech.ac.in>
2024-10-15 01:15:56 +0530

    Weather app

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const cityInput = document.getElementById("city-input");
+    const getWeatherBtn = document.getElementById("get-weather-btn");
+    const weatherInfo = document.getElementById("weather-info");
+    const cityDisplay = document.getElementById("city-name");
+    const tempratureDisplay = document.getElementById("temperature");
+    const descriptionDisplay = document.getElementById("description");
+    const errorMessage = document.getElementById("error-message");
+    const API_KEY = "a68067dc2184a2b23e10f26ca8292348";
+
+    getWeatherBtn.addEventListener('click', async () => {
+        const city = cityInput.value.trim();
+        if (!city) return;
+        try {
+            const weatherdata = await fetchWeatherData(city);
+            displayWeatherData(weatherdata);
+        } catch (error) {
+            showError();
+        }
+    });
+
+    async function fetchWeatherData(city) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("City not Found");
+        }
+        const data = await response.json();
+        return data;
+    }
+
+    function displayWeatherData(data) {
+        const { name, main, weather } = data;
+        cityDisplay.textContent = name;
+        tempratureDisplay.textContent = `Temperature: ${main.temp}°C`;
+        descriptionDisplay.textContent = `Weather: ${weather[0].description}`;
+        weatherInfo.classList.remove("hidden");
+        errorMessage.classList.add("hidden");
+    }
+
+    function showError() {
+        weatherInfo.classList.add("hidden");
+        errorMessage.classList.remove("hidden");
+    }
+});
